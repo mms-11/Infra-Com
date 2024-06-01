8bd5497d29a97aa925534b2c21bb102dbae181c7
mms-11 <140762703+mms-11@users.noreply.github.com>
2024-06-01 06:19:25 -0300

    adicionando udp do cliente

---
+++ entrega1/cliente.py
@@ -0,0 +1,24 @@
+import socket
+import time
+
+class udpCliente():
+    def __init__(self,sktFam,sktType,sktBinding, maxBuff):
+        self.socket = socket.socket(sktFam,sktType)
+        self.socket.bind(sktBinding)
+        self.socket.settimeout(0.1)
+
+        if self.socket is None:
+            raise "SOCKET NÃO DISPONÍVEL"
+        self.maxBuff = maxBuff
+        self.init_trans = 0
+        self.stop = 0
+
+        def listen(self):
+            while True:
+                try:
+                    data, fonte = self.socket.recvfrom(self, maxBuff)
+                except: 
+                    continue
+            def send(self, end_servidor, porta_servidor, msg):
+                self.socket.sendto(msg,end_servidor,porta_servidor)
+                time.sleep(0.001)
